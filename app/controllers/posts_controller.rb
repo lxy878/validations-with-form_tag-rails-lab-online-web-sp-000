d7daa469b1b9f8fbd5ca3cdfbe4644987cf46711
Xiaoyi Liu <lxy878@gmail.com>
2020-06-21 14:52:01 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
+    binding.pry
     if @post.update(post_params)
       redirect_to post_path(@post)
     else
