08d2fdfcc5a05e1b1cc7b1b7d7a66ad636b8c2ce
Xiaoyi Liu <lxy878@gmail.com>
2020-06-21 14:08:28 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :authors, :posts, only: [:show, :new, :edit, :create]
+  resource :authors, only: [:show, :new, :edit, :create], :posts
 end
